55d5b2926b45d140e0807f5dfc2d39d16c2a755a
gussiciliano <gussiciliano@gmail.com>
2018-04-13 01:12:24 -0300

    adding spring files

---
+++ src/main/java/com/unla/ghsicilianotfi/GhsicilianotfiApplication.java
@@ -0,0 +1,12 @@
+package com.unla.ghsicilianotfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhsicilianotfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhsicilianotfiApplication.class, args);
+	}
+}
