287f2090bb6f3e86ce222843c68f19dae8586852
gussiciliano <gussiciliano@gmail.com>
2018-04-22 17:09:56 -0300

    add template

---
+++ src/main/java/com/unla/ghsicilianotfi/contollers/HomeController.java
@@ -0,0 +1,15 @@
+package com.unla.ghsicilianotfi.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@GetMapping("")
+	public String index() {
+		return "home/index";
+	}
+}
