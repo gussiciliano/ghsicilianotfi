119590a9952dc73cd1273741b2d0ad2c3529e468
gussiciliano <gussiciliano@gmail.com>
2018-04-22 21:19:49 -0300

    add get examples

--- src/main/java/com/unla/ghsicilianotfi/contollers/HomeController.java
+++ src/main/java/com/unla/ghsicilianotfi/contollers/HomeController.java
@@ -2,14 +2,45 @@ package com.unla.ghsicilianotfi.contollers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-	@GetMapping("")
+	//GET Example: SERVER/index
+	@GetMapping("/index")
 	public String index() {
 		return "home/index";
 	}
+	
+	//GET Example: SERVER/hello?name=someName
+	@GetMapping("/hello")
+	public ModelAndView helloParams1(@RequestParam(name="name", required=false, defaultValue="null") String name) {
+		ModelAndView mV = new ModelAndView("home/hello");
+		mV.addObject("name", name);
+		return mV;
+	}
+	
+	//GET Example: SERVER/hello/someName
+	@GetMapping("/hello/{name}")
+	public ModelAndView helloParams2(@PathVariable("name") String name) {
+		ModelAndView mV = new ModelAndView("home/hello");
+		mV.addObject("name", name);
+		return mV;
+	}
+	
+	@GetMapping("/")
+	public RedirectView redirectToHomeIndex() {
+		return new RedirectView("/index");
+	}
 }
+
+
+
+
+
